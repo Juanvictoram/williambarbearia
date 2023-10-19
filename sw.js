35c179e80ce87e8dc7cff2274431c50926b73b67
juan victor soares amaral <119392809+Juanvictoram@users.noreply.github.com>
2023-10-19 02:48:04 -0400

    app commit

---
+++ sw.js
@@ -0,0 +1,48 @@
+const cacheName = 'tributos mega reino'
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            cache.addAll([
+                './',
+                './index.hml',
+                './manifest.webmanifest',
+                "./index.js"
+            ])
+        })
+    )
+    return self.skipWaiting()
+})
+self.addEventListener('actvate', e =>{
+    self.clients.claim()
+})
+
+self.addEventListener('fetch', async e =>{
+    const req = e.request
+    const url = new URL(req.url)
+
+    if(url.login === location.origin){
+        e.respondWith(cacheFirst(req))
+    } else {
+        e.respondWith(networkAndCache(req))
+    }
+})
+
+async function cacheFirst(req){
+    const cache = await caches.open(cacheName)
+    const cached = await cache.match(req)
+
+    return cached ||fetch(req)
+}
+
+async function networkAndCache(req){
+    const cache = await caches.open(cacheName)
+    try{
+        const refresh = await fetch(req)
+        await cache.put(req, refresh.clone())
+        return refresh
+    } catch(e){
+        const cached = await cache.match(req)
+        return cached
+    }
+}
